8d16b02f8a366c91e9340bd3991952499915f268
David M. Lary <dmlary@gmail.com>
2023-08-05 13:12:21 -0500

    Switched to ViewNode approach

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,6 @@
 /// vertex buffers, or anything else in this example.  Effectively it is
 /// shader-toy written in bevy.
 ///
-/// This example runs `shader.wgsl` shader each frame.  The ShaderToy component
-/// holds the handle to the shader and it's passed through the ShaderToyPipeline
-/// to render. If you see a 3d cube, check the console output for the shader
-/// error.
-///
 /// If no messages appear on stdout, set to help debug:
 ///     RUST_LOG="info,wgpu_core=warn,wgpu_hal=warn"
 ///
@@ -16,63 +11,43 @@
 ///
 use bevy::{
     asset::ChangeWatcher,
-    core_pipeline::core_3d::Transparent3d,
-    ecs::{query::ROQueryItem, system::SystemParamItem},
+    core_pipeline::core_3d,
+    ecs::query::QueryItem,
     prelude::*,
     render::{
-        render_phase::{
-            AddRenderCommand, DrawFunctions, PhaseItem, RenderCommand, RenderCommandResult,
-            RenderPhase, SetItemPipeline,
+        camera::ExtractedCamera,
+        render_graph::{RenderGraphApp, ViewNode, ViewNodeRunner},
+        render_resource::{
+            BlendState, CachedRenderPipelineId, ColorTargetState, ColorWrites, FragmentState,
+            LoadOp, MultisampleState, Operations, PipelineCache, PolygonMode, PrimitiveState,
+            PrimitiveTopology, RenderPassDescriptor, RenderPipelineDescriptor, TextureFormat,
         },
-        render_resource::*,
         texture::BevyDefault,
-        view::VisibleEntities,
-        Extract, Render, RenderApp, RenderSet,
+        view::ViewTarget,
+        RenderApp,
     },
 };
-use std::{borrow::Cow, time::Duration};
+use std::time::Duration;
 
 fn main() {
     let mut app = App::new();
 
     // enable hot-loading so our shader gets reloaded when it changes
-    app.add_plugins((DefaultPlugins.set(AssetPlugin {
-        watch_for_changes: ChangeWatcher::with_delay(Duration::from_secs(1)),
-        ..default()
-    }),))
-        .add_systems(Startup, setup);
-
-    // Add rendering of ShaderToys to the RenderApp
-    let render_app = app.get_sub_app_mut(RenderApp).unwrap();
-    render_app
-        // the ShaderToyPipeline is used to generate RenderPipelineDescriptor
-        // for each shader; see `RenderPipelineDescriptor::specialize()` calls
-        // in `queue_shader_toys()`.
-        .init_resource::<ShaderToyPipeline>()
-        // This is just a cache for pipelines created by the above.
-        //
-        // NOTE: If your shader is not configurable (requires no
-        // specialization), you may be able to use
-        // `PipelineCache::queue_render_pipeline()` with a
-        // RenderPipelineDescriptor and skip the specialization code.
-        .init_resource::<SpecializedRenderPipelines<ShaderToyPipeline>>()
-        // This registers DrawShaderToy as a render command in the Transparent3d
-        // render pass; it's used in `queue_shader_toys`
-        .add_render_command::<Transparent3d, DrawShaderToy>()
-        // This copies ShaderToy instances from the World into the render World
-        .add_systems(ExtractSchedule, extract_shader_toys)
-        // This adds DrawShaderToy commands to the Transparent3d RenderPhase for
-        // each ShaderToy in the render World.
-        .add_systems(Render, queue_shader_toys.in_set(RenderSet::Queue));
-
-    app.run();
+    app.add_plugins((
+        DefaultPlugins.set(AssetPlugin {
+            watch_for_changes: ChangeWatcher::with_delay(Duration::from_secs(1)),
+            ..default()
+        }),
+        HexGridPlugin,
+    ))
+    .add_systems(Startup, setup)
+    .run();
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
 ) {
     // add a cube so it's really clear when the shader doesn't run
     commands.spawn(MaterialMeshBundle {
@@ -87,49 +62,101 @@ fn setup(
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-
-    // add our shader toy; note that the SpatialBundle is required so that the
-    // ShaderToy is visible to the camera.  This can probably be changed by
-    // tweaking `queue_shader_toys()`
-    let shader = asset_server.load("shader.wgsl");
-    commands.spawn((SpatialBundle::default(), ShaderToy { shader }));
 }
 
-#[derive(Component, Clone)]
-struct ShaderToy {
-    shader: Handle<Shader>,
+struct HexGridPlugin;
+
+impl Plugin for HexGridPlugin {
+    fn build(&self, app: &mut App) {
+        let render_app = app
+            .get_sub_app_mut(RenderApp)
+            .expect("RenderApp should already exist in App");
+
+        // add our post-processing render node to the render graph
+        // place it between tonemapping & the end of post-processing shaders
+        render_app
+            .add_render_graph_node::<ViewNodeRunner<HexGridRenderNode>>(
+                core_3d::graph::NAME,
+                HexGridRenderNode::NAME,
+            )
+            .add_render_graph_edges(
+                core_3d::graph::NAME,
+                &[
+                    core_3d::graph::node::TONEMAPPING,
+                    HexGridRenderNode::NAME,
+                    core_3d::graph::node::END_MAIN_PASS_POST_PROCESSING,
+                ],
+            );
+    }
+
+    fn finish(&self, app: &mut App) {
+        let render_app = app
+            .get_sub_app_mut(RenderApp)
+            .expect("RenderApp should already exist in App");
+        render_app.init_resource::<HexGridPipeline>();
+    }
 }
 
-#[derive(Resource, Default)]
-struct ShaderToyPipeline;
+#[derive(Debug, Default)]
+struct HexGridRenderNode;
 
-impl SpecializedRenderPipeline for ShaderToyPipeline {
-    type Key = Handle<Shader>;
+impl HexGridRenderNode {
+    pub const NAME: &str = "hex_grid";
+}
 
-    fn specialize(&self, key: Self::Key) -> RenderPipelineDescriptor {
-        // For more details about this struct, look at the learning wgpu
-        // tutorials.
-        RenderPipelineDescriptor {
-            // probably should change this name to be based on the Key
-            label: Some(Cow::Borrowed("shader-toy-pipeline")),
+impl ViewNode for HexGridRenderNode {
+    type ViewQuery = (&'static ExtractedCamera, &'static ViewTarget);
+    fn run(
+        &self,
+        graph: &mut bevy::render::render_graph::RenderGraphContext,
+        render_context: &mut bevy::render::renderer::RenderContext,
+        (camera, target): QueryItem<Self::ViewQuery>,
+        world: &World,
+    ) -> Result<(), bevy::render::render_graph::NodeRunError> {
+        let hex_grid_pipeline = world.resource::<HexGridPipeline>();
+        let pipeline_cache = world.resource::<PipelineCache>();
+        let pipeline = pipeline_cache
+            .get_render_pipeline(hex_grid_pipeline.pipeline_id)
+            .expect("HexGridPipeline should be present in the PipelineCache");
+
+        let mut render_pass = render_context.begin_tracked_render_pass(RenderPassDescriptor {
+            label: Some("hex_grid_pass"),
+            color_attachments: &[Some(target.get_color_attachment(Operations {
+                load: LoadOp::Load,
+                store: true,
+            }))],
+            depth_stencil_attachment: None,
+        });
+
+        render_pass.set_render_pipeline(pipeline);
+        render_pass.draw(0..4, 0..1);
+        debug!("{:?} ran", self);
+        Ok(())
+    }
+}
+
+#[derive(Debug, Resource)]
+struct HexGridPipeline {
+    pipeline_id: CachedRenderPipelineId,
+}
+
+impl FromWorld for HexGridPipeline {
+    fn from_world(world: &mut World) -> Self {
+        let shader = world.resource::<AssetServer>().load("hex_grid.wgsl");
+
+        let pipeline_cache = world.resource_mut::<PipelineCache>();
+
+        let pipeline_id = pipeline_cache.queue_render_pipeline(RenderPipelineDescriptor {
+            label: Some("hex_grid_pipeline".into()),
             layout: vec![],
             push_constant_ranges: Vec::new(),
-            vertex: VertexState {
-                shader: key.clone(),
+            vertex: bevy::render::render_resource::VertexState {
+                shader: shader.clone(),
                 shader_defs: vec![],
-                entry_point: Cow::Borrowed("vertex"),
+                entry_point: "vertex".into(),
                 buffers: vec![],
             },
-            fragment: Some(FragmentState {
-                shader: key,
-                shader_defs: vec![],
-                entry_point: Cow::Borrowed("fragment"),
-                targets: vec![Some(ColorTargetState {
-                    format: TextureFormat::bevy_default(),
-                    blend: Some(BlendState::ALPHA_BLENDING),
-                    write_mask: ColorWrites::ALL,
-                })],
-            }),
+            // default does not work here as we're using TriangleStrip
             primitive: PrimitiveState {
                 topology: PrimitiveTopology::TriangleStrip,
                 strip_index_format: None,
@@ -139,115 +166,31 @@ impl SpecializedRenderPipeline for ShaderToyPipeline {
                 polygon_mode: PolygonMode::Fill,
                 conservative: false,
             },
-            // NOTE: `depth_stencil` & `multisample` fields must have the same
-            // settings as tne `RenderPass` (`Transparent3d`), otherwise the
-            // shader will fail to compile.
-            depth_stencil: Some(DepthStencilState {
-                format: TextureFormat::Depth32Float,
-                depth_write_enabled: true,
-                depth_compare: CompareFunction::Greater,
-                stencil: StencilState {
-                    front: StencilFaceState::IGNORE,
-                    back: StencilFaceState::IGNORE,
-                    read_mask: 0,
-                    write_mask: 0,
-                },
-                bias: DepthBiasState {
-                    constant: 0,
-                    slope_scale: 0.0,
-                    clamp: 0.0,
-                },
-            }),
+            depth_stencil: None,
+            // XXX hard-coding this for right now, but this is wrong.  The post
+            // processing example uses default() and works fine.  But when we
+            // try to run our pipeline, we get errors about msaa mismatch.
+            // One key difference is that we're not calling
+            // view_target.post_process_write() in our `run()`.
+            //
+            // multisample: MultisampleState::default(),
             multisample: MultisampleState {
                 count: 4,
                 mask: !0,
                 alpha_to_coverage_enabled: false,
             },
-        }
-    }
-}
+            fragment: Some(FragmentState {
+                shader,
+                shader_defs: vec![],
+                entry_point: "fragment".into(),
+                targets: vec![Some(ColorTargetState {
+                    format: TextureFormat::bevy_default(),
+                    blend: Some(BlendState::ALPHA_BLENDING),
+                    write_mask: ColorWrites::ALL,
+                })],
+            }),
+        });
 
-// This is used to construct the series of RenderCommands used to draw the
-// ShaderToy.  We registered this in main() for Transparent3d phase, and we
-// use it in `queue_shader_toys()` as the `Transparent3d.draw_command`.
-//
-// - SetItemPipeline is a bevy RenderCommand for setting the render pipeline.
-// - FinishDrawShaderToy is our RenderCommand for issuing the final draw() call.
-type DrawShaderToy = (SetItemPipeline, FinishDrawShaderToy);
-
-struct FinishDrawShaderToy;
-
-impl<P: PhaseItem> RenderCommand<P> for FinishDrawShaderToy {
-    type Param = ();
-    type ViewWorldQuery = ();
-    type ItemWorldQuery = ();
-
-    #[inline]
-    fn render<'w>(
-        _item: &P,
-        _view: ROQueryItem<'w, Self::ViewWorldQuery>,
-        _entity: ROQueryItem<'w, Self::ItemWorldQuery>,
-        _param: SystemParamItem<'w, '_, Self::Param>,
-        pass: &mut bevy::render::render_phase::TrackedRenderPass<'w>,
-    ) -> RenderCommandResult {
-        // Simple draw command, giving vertices 0..4, and only one instance 0.
-        // These values are passed to the shader.  See wgpu tutorial, or opengl
-        // tutorial for more details on what this means
-        pass.draw(0..4, 0..1);
-        RenderCommandResult::Success
-    }
-}
-
-/// copy ShaderToy instances from World into the render World
-///
-/// Rendering is done against the render world instead of the live world.  See
-/// bevy 0.6 upgrade notes for description of why (spoilers: performance).
-fn extract_shader_toys(mut commands: Commands, shader_toys: Extract<Query<(Entity, &ShaderToy)>>) {
-    let extracted: Vec<(Entity, ShaderToy)> = shader_toys
-        .iter()
-        .map(|(entity, shader_toy)| (entity, shader_toy.clone()))
-        .collect();
-    commands.insert_or_spawn_batch(extracted);
-}
-
-/// Take the ShaderToy instances in the render world and add draw items to the
-/// Transparent3d phase for any that are visible.
-fn queue_shader_toys(
-    pipeline_cache: ResMut<PipelineCache>,
-    mut pipelines: ResMut<SpecializedRenderPipelines<ShaderToyPipeline>>,
-    pipeline: Res<ShaderToyPipeline>,
-    transparent_draw_functions: Res<DrawFunctions<Transparent3d>>,
-    mut views: Query<(&VisibleEntities, &mut RenderPhase<Transparent3d>)>,
-    shader_toys: Query<&ShaderToy>,
-) {
-    // look up our draw function
-    let draw_function = transparent_draw_functions
-        .read()
-        .get_id::<DrawShaderToy>()
-        .unwrap();
-
-    // go through each view (these can be equivalent to cameras)
-    for (entities, mut phase) in &mut views {
-        // go through all of the visibile entities in the view
-        for &entity in &entities.entities {
-            // if we have a shader entity visibile to the view...
-            let Ok(shader_toy) = shader_toys.get(entity) else { continue; };
-
-            // specialize the pipeline for the specific shader in the ShaderToy
-            let pipeline =
-                pipelines.specialize(&pipeline_cache, &pipeline, shader_toy.shader.clone());
-            // Add the pipeline & draw function `DrawShaderToy` to the
-            // Transparent3d phase for rendering.
-            //
-            // When Transparent3d runs, it will execute the `RenderCommand`s in
-            // `DrawShaderToy` using the pipeline provided (the shader in
-            // `ShaderToy`).
-            phase.items.push(Transparent3d {
-                pipeline,
-                draw_function,
-                entity,
-                distance: f32::NEG_INFINITY,
-            });
-        }
+        Self { pipeline_id }
     }
 }
